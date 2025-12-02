4487e9dbcf565bc80ac69efb0ce7dea02caf754d
Youngho Kwon <dy-08@naver.com>
2025-12-02 13:03:53 +0900

    invalidateQueries 업데이트

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -11,10 +11,10 @@ export default function Products() {
     } = useQuery({
         queryKey: ['products', checked],
         queryFn: async () => {
-            console.log('fetching....');
+            console.log('fetching....', checked);
             return fetch(`data/${checked ? 'sale_' : ''}products.json`).then((res) => res.json());
         },
-        staleTime: 1000 * 60 * 5,
+        staleTime: 1000 * 5,
     });
     const handleChange = () => setChecked((prev) => !prev);
 
